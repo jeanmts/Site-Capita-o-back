85429f51e3fa7d39ee519fa00d4b3dad972aabed
Jean mato <matos.sd52@gmail.com>
2023-07-20 20:16:07 -0300

    endpoint servidor

--- src/controllers/Info.js
+++ src/controllers/Info.js
@@ -6,7 +6,7 @@ const dados = async ( req ,res) => {
         if (!nome || !telefone || !cpf || !email) {
             res.json({message: "Todos os campos são obrigatorios"})
         }
-        const query = "insert into dadosDoUsuario (nome, email, telefone, cpf) values ($1, $2, $3, $4)";
+        const query = "insert into dadosdousuario (nome, email, telefone, cpf) values ($1, $2, $3, $4)";
         const params = [nome, email, telefone, cpf];
         const record = await pool.query(query, params);
         res.status(200).json({ message: "Cadastro concluido"})
@@ -18,7 +18,7 @@ const dados = async ( req ,res) => {
 
 const usuarios = async (req, res) => {
     try {
-        const query  = "select * from dadosDoUsuario"
+        const query  = "select * from dadosdousuario"
         const {rows} = await pool.query(query)
         res.status(200).json(rows)
     } catch (error) {
