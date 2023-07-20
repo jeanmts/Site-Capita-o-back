6a46f0c302bf9bbf2c71c86f14e4df92952be13d
Jean mato <matos.sd52@gmail.com>
2023-07-20 18:04:01 -0300

    endpoint servidor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const routes = require('./router/routes.js')
 const express = require('express')
 const app = express();
+const cors =  require('cors');
 
 
 app.use(express.json());
 app.use(routes)
+app.use(cors())
 app.listen(3000);
