3e59c1aa3ea98012ec58a11b919dbd6b5f2c4042
Jean mato <matos.sd52@gmail.com>
2023-07-22 18:34:53 -0300

    cors adicionado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const routes = require('./router/routes.js')
-const express = require('express')
+require('dotenv').config();
+
+const routes = require('./router/routes.js');
+const express = require('express');
 const app = express();
 const cors =  require('cors');
 
 
+app.use(cors());
 app.use(express.json());
-app.use(routes)
-app.use(cors())
+app.use(routes);
 app.listen(3000);
